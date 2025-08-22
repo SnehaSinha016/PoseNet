17d2c5888fc0fce06457cade4f50e3e8306da701
SnehaSinha016 <sneha.sinha1610@gmail.com>
2025-08-23 04:53:56 +0530

    commit

--- client/src/components/LoginUp.jsx
+++ client/src/components/LoginUp.jsx
@@ -32,15 +32,15 @@ const LoginUp = () => {
         }
   return (
     <div className="h-screen w-screen flex flex-col items-end justify-center py-10 pr-80 bg-cover bg-center absolute" style={{backgroundImage:`url(${Login})`}}>
-      <div className="w-[1000px] h-[900px]  border-gray-300 rounded-lg p-6 bg-white shadow-lg  ">
-        <h1 className="text-[180px] font-bold text-gray-800 text-center font-rustic">Postura</h1>
-<p className="text-5xl text-gray-500 text-center mb-20">AI That Has Your Back.</p>
-        <form className="space-y-10 " onSubmit={handleSubmit}>
+      <div className="w-[400px] h-[370px]  border-gray-300 rounded-lg p-6 bg-white shadow-lg  ">
+        <h1 className="text-[70px] font-bold text-gray-800 text-center font-rustic">Postura</h1>
+<p className="text-2xl text-gray-500 text-center mb-15">AI That Has Your Back.</p>
+        <form className="space-y-3 " onSubmit={handleSubmit}>
           <input
             type="email"
             required
             placeholder="Email"
-            className="w-full h-[100px] text-4xl px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
+            className="w-full h-[30px] text-2xl px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
             value={Email}
             onChange={(e)=>setEmail(e.target.value)}
             autoComplete="email"
@@ -52,19 +52,19 @@ const LoginUp = () => {
             value={Password}
             onChange={(e)=>setPassword(e.target.value)}
             autoComplete="current-password"
-            className="w-full h-[100px] text-4xl px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
+            className="w-full h-[30px] text-2xl px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
           />
           <button
             type="submit"
-            className="w-[300px] h-[70px] mt-[5%] text-5xl bg-gradient-to-r from-blue-600 to-blue-300 text-white py-2 font-semibold hover:bg-blue-600 rounded-3xl ml-[38%]"
+            className="w-[100px] h-[30px]  text-5xl bg-gradient-to-r from-blue-600 to-blue-300 text-white py-2 font-semibold hover:bg-blue-600 rounded-3xl ml-[38%%]"
             onClick={navigate('/Home')}>
             Log in
           </button>
         </form>
       </div>
-      <div className="w-[1000px] h-[200px] border border-gray-300 rounded-lg p-6 bg-white shadow-sm  mt-[30px]">
-        <p className="text-5xl font-bold text-center">Don't have an account?</p>
-        <button className="text-4xl font-bold text-blue-500 hover:text-purple-400 text-center mt-[10px] ml-[45%]" onClick={()=>navigate('/')}>Sign up</button>
+      <div className="w-[400px] h-[100px] border border-gray-300 rounded-lg p-6 bg-white shadow-sm  mt-[30px]">
+        <p className="text-2xl font-bold text-center">Don't have an account?</p>
+        <button className="text-xl font-bold text-blue-500 hover:text-purple-400 text-center mt-[1px] ml-[40%]" onClick={()=>navigate('/')}>Sign up</button>
       </div>
     </div>
   );
--- client/src/components/signUp.jsx
+++ client/src/components/signUp.jsx
@@ -29,15 +29,15 @@ const Signup = () => {
   }
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-100 to-purple-100 py-10">
-      <div className="w-[1000px] h-[1500px] border border-gray-300 rounded-3xl p-6 bg-white shadow-lg items-center justify-center  ">
-        <h1 className="text-[180px] font-bold text-gray-800 text-center font-rustic">Postura</h1>
-<p className="text-6xl text-gray-500 text-center mb-20">AI That Has Your Back.</p>
-        <form className="space-y-10" onSubmit={handleSubmit}>
+      <div className="w-[400px] h-[370px] border border-gray-300 rounded-3xl p-6 bg-white shadow-lg items-center justify-center  ">
+        <h1 className="text-[70px] font-bold text-gray-800 text-center font-rustic">Postura</h1>
+<p className="text-2xl text-gray-500 text-center mb-15">AI That Has Your Back.</p>
+        <form className="space-y-3 " onSubmit={handleSubmit}>
           <input
             type="email"
             required
             placeholder=" Email"
-            className="w-full h-[100px] text-4xl px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
+            className="w-full h-[30px] text-2xl px-3 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
              value={Email}
             onChange={(e) => setEmail(e.target.value)}
             autoComplete="email"
@@ -46,7 +46,7 @@ const Signup = () => {
             type="password"
             required
             placeholder="Password"
-            className="w-full h-[100px] text-4xl px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
+            className="w-full h-[30px] text-2xl px-3  border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
              value={Password}
             onChange={(e) => setPassword(e.target.value)}
             autoComplete="new-password"
@@ -55,7 +55,7 @@ const Signup = () => {
             type="text"
             required
             placeholder="Full Name"
-            className="w-full h-[100px] text-4xl px-3 py-2 border border-gray-300  focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
+            className="w-full h-[30px] text-2xl px-3  border border-gray-300  focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
              value={Fullname}
             onChange={(e) => setFullname(e.target.value)}
             autoComplete="name"
@@ -64,22 +64,22 @@ const Signup = () => {
             type="text"
             required
             placeholder="Username"
-            className="w-full h-[100px] text-4xl px-3 py-2 border border-gray-300   focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
+            className="w-full h-[30px] text-2xl px-3  border border-gray-300   focus:outline-none focus:ring-1 focus:ring-blue-400 rounded-2xl"
               value={UserName}
             onChange={(e) => setUserName(e.target.value)}
             autoComplete="username"
           />
           <button
             type="submit"
-            className="w-[300px] h-[100px] mt-[15%] text-5xl bg-gradient-to-r from-blue-800 to-blue-400 text-white py-2 font-semibold hover:bg-blue-600 rounded-3xl ml-[30%]"
+            className="w-[80px] h-[30px] mt-[15%] text-1xl bg-gradient-to-r from-blue-800 to-blue-400 text-white py-2 font-semibold hover:bg-blue-600 rounded-3xl ml-[30%]"
           >
             Sign up
           </button>
         </form>
       </div>
-      <div className="w-[1000px] h-[200px] border border-gray-300 rounded-lg p-6 bg-white shadow-lg  mt-[30px]">
-        <p className="text-5xl font-bold text-center">Have an account?</p>
-        <button className="text-4xl font-bold text-blue-500 hover:text-blue-700 text-center  mt-[10px] " onClick={()=>navigate('/LoginUp')}>Log in</button>
+      <div className="w-[400px] h-[100px] border border-gray-300 rounded-lg p-6 bg-white shadow-lg  mt-[30px]">
+        <p className="text-2xl font-bold text-center">Have an account?</p>
+        <button className="text-1xl font-bold text-blue-500 hover:text-blue-700 text-center  " onClick={()=>navigate('/LoginUp')}>Log in</button>
       </div>
     </div>
   );
