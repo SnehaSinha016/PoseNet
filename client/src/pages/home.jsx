6dcb160289310113f9ac81b99c7d848f313c1d46
SnehaSinha016 <sneha.sinha1610@gmail.com>
2025-08-23 05:20:51 +0530

    commit

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -5,7 +5,7 @@ import Gettingstarted from '../components/gettingstarted'
 import Navbar from '../components/Navbar'
 const home = () => {
   return (
-    <div className="w-screen h-screen flex flex-col">
+    <div className="w-screen h-screen flex ">
       <Navbar/>
       <div className="w-1/2 p-0 m-0"><Blog/></div>
       <div className="w-1/2 p-0 m-0 "><Gettingstarted/></div>
